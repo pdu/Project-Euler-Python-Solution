a6a9b995b8fbdf4f9388e5d968883791fad17213
pdu <peng.du.hust@gmail.com>
2011-03-04 00:23:11 +0800

    Solve problem 18, 67: same problem description different data range, dynamic programming

---
+++ 1-100/18.py
@@ -0,0 +1,21 @@
+# Problem link: http://projecteuler.net/index.php?section=problems&id=18
+
+import sys
+
+# read the input
+L = []
+while True:
+    line = sys.stdin.readline()
+    if not line:
+        break
+    M = []
+    for s in line.split(' '):
+        M.append(int(s))
+    L.append(M)
+
+# dynamic programming, calculate from bottom to up
+for i in range(len(L) - 2, -1, -1):
+    for j in range(0, len(L[i])):
+        L[i][j] += max(L[i + 1][j], L[i + 1][j + 1])
+print L[0][0]
+
